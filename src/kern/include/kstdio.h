f07325aedac559741799610303aa145af4c0d5bc
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-01 02:45:15 +0600

    kprintf() - reverted endString parameter addition

--- src/kern/include/kstdio.h
+++ src/kern/include/kstdio.h
@@ -1,7 +1,7 @@
 #ifndef __KSTDIO_H
 #define __KSTDIO_H
 #include <stdint.h>
-void kprintf(uint8_t*,uint8_t*,uint8_t*); 
+void kprintf(uint8_t*,uint8_t*); 
 void kscanf(uint8_t*,uint8_t *);
 
 #endif /* KSTDIO */
--- src/kern/kmain/kmain.c
+++ src/kern/kmain/kmain.c
@@ -14,137 +14,137 @@ void demo_kprintf_kscanf() {
 	float inputFloat2;
 	float sumFloat;
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"[DEMO] kscanf() & kprintf()", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"[INPUT]", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[DEMO] kscanf() & kprintf()\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[INPUT]");
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A String", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A String");
 	kscanf((uint8_t*)"%s", inputString);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[STRING]", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)&inputString, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[STRING]");
+	kprintf((uint8_t*)"%s",(uint8_t*)&inputString);
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Character", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Character");
 	kscanf((uint8_t*)"%c", (uint8_t*)&inputCharacter);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[CHARACTER]", "\n");
-	kprintf((uint8_t*)"%c",(uint8_t*)&inputCharacter, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[CHARACTER]");
+	kprintf((uint8_t*)"%c",(uint8_t*)&inputCharacter);
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Decimal Number", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Decimal Number");
 	kscanf((uint8_t*)"%d", (uint8_t*)&inputDecimal);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[DECIMAL]", "\n");
-	kprintf((uint8_t*)"%d",(uint8_t*)&inputDecimal, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[DECIMAL]");
+	kprintf((uint8_t*)"%d",(uint8_t*)&inputDecimal);
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Hexadecimal Number", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Hexadecimal Number");
 	kscanf((uint8_t*)"%x", (uint8_t*)&inputHexadecimal);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[HEXADECIMAL]", "\n");
-	kprintf((uint8_t*)"%x",(uint8_t*)&inputHexadecimal, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[HEXADECIMAL]");
+	kprintf((uint8_t*)"%x",(uint8_t*)&inputHexadecimal);
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Floating Point Number", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter A Floating Point Number");
 	kscanf((uint8_t*)"%f", (uint8_t*)&inputFloat1);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[FLOAT]", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat1, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[FLOAT]");
+	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat1);
 		
-	kprintf((uint8_t*)"%s",(uint8_t*)"Enter Another Floating Point Number", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Enter Another Floating Point Number");
 	kscanf((uint8_t*)"%f", (uint8_t*)&inputFloat2);
-	kprintf((uint8_t*)"%s",(uint8_t*)"[FLOAT]", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat2, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[FLOAT]");
+	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat2);
 
 	// float addition
-	kprintf((uint8_t*)"%s",(uint8_t*)"[FPU]", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"Input Float 1:", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat1, "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"Input Float 2:", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat2, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"[FPU]");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Input Float 1:");
+	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat1);
+	kprintf((uint8_t*)"%s",(uint8_t*)"Input Float 2:");
+	kprintf((uint8_t*)"%f",(uint8_t*)&inputFloat2);
 
 	sumFloat = inputFloat1 + inputFloat2;
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Summation:", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&sumFloat, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Summation:");
+	kprintf((uint8_t*)"%f",(uint8_t*)&sumFloat);
 
 	float divFloat = inputFloat1 / inputFloat2;
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"Division:", "\n");
-	kprintf((uint8_t*)"%f",(uint8_t*)&divFloat, "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Division:");
+	kprintf((uint8_t*)"%f",(uint8_t*)&divFloat);
 
-	kprintf((uint8_t*)"%s",(uint8_t*)"-------------------------------------------\n", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"-------------------------------------------\n");
 }
 
-// void test(void){
-// 	/** Test Case: print countVal and countFlag until it is set **/
+void test(void){
+	/** Test Case: print countVal and countFlag until it is set **/
 
-// 	SysTick_disable();
-// 	SysTick_enable();
+	SysTick_disable();
+	SysTick_enable();
 	
-// 	uint8_t outputLine[200];
-// 	uint8_t stkValString[200];
-// 	uint8_t msCountString[200];
+	uint8_t outputLine[200];
+	uint8_t stkValString[200];
+	uint8_t msCountString[200];
 
-// 	uint8_t countFlag = SysTick_getStatus();
-// 	uint32_t stkVal = SysTick_getCount();
-// 	uint32_t msCount = SysTick_getTime();
+	uint8_t countFlag = SysTick_getStatus();
+	uint32_t stkVal = SysTick_getCount();
+	uint32_t msCount = SysTick_getTime();
 
-// 	while(1) {
-// 		intToString(stkValString, stkVal);
-// 		intToString(msCountString, msCount);
+	while(1) {
+		intToString(stkValString, stkVal);
+		intToString(msCountString, msCount);
 
-// 		outputLine[0] = '';
-// 		concatString(outputLine[1], )
+		outputLine[0] = '';
+		concatString(outputLine[1], )
 
 
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"SysTick VAl : ");
-// 		kprintf((uint8_t *)"%d",(uint8_t *) &tickCount);
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"mscount : ");
-// 		kprintf((uint8_t *)"%d",(uint8_t *) &timeMS);
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"COUNTFLAG : 0");
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"SysTick VAl : ");
+		kprintf((uint8_t *)"%d",(uint8_t *) &tickCount);
+		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"mscount : ");
+		kprintf((uint8_t *)"%d",(uint8_t *) &timeMS);
+		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"COUNTFLAG : 0");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
 
-// 		if(countFlag==1) {
-// 			kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
-// 			break;
-// 		}
+		if(countFlag==1) {
+			kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
+			break;
+		}
 
-// 		countFlag = SysTick_getStatus();
-// 		stkVal = SysTick_getCount();
-// 		msCount = SysTick_getTime();
-// 	}
-// }
+		countFlag = SysTick_getStatus();
+		stkVal = SysTick_getCount();
+		msCount = SysTick_getTime();
+	}
+}
 
-// void test2(uint32_t setTimeMS){
-// 	/** Count upto TimeMS **/
+void test2(uint32_t setTimeMS){
+	/** Count upto TimeMS **/
 
-// 	SysTick_Disable();
-// 	SysTick_Enable();
+	SysTick_Disable();
+	SysTick_Enable();
 	
-// 	uint8_t stat = SysTick_GetStatus() + '0';
-// 	uint32_t tickCount = SysTick_GetCount();		// Expected to start from LOAD VALUE
-// 	uint32_t timeMS = SysTickGetTime();				// Expected to start from 0
-// 	// systick delay
-// 	while(timeMS <= setTimeMS){
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"SysTick VAl : ");
-// 		kprintf((uint8_t *)"%d",(uint8_t *) &tickCount);
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"mscount : ");
-// 		kprintf((uint8_t *)"%d",(uint8_t *) &timeMS);
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"COUNTFLAG : ");
-// 		kprintf((uint8_t *)"%c",(uint8_t *) &stat);
-// 		kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
-// 		stat = SysTick_GetStatus() + '0';
-// 		tickCount = SysTick_GetCount();
-// 		timeMS = SysTickGetTime();
-// 	}	
-// 	kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
-// }
+	uint8_t stat = SysTick_GetStatus() + '0';
+	uint32_t tickCount = SysTick_GetCount();		// Expected to start from LOAD VALUE
+	uint32_t timeMS = SysTickGetTime();				// Expected to start from 0
+	// systick delay
+	while(timeMS <= setTimeMS){
+		kprintf((uint8_t *)"%s",(uint8_t *) &"SysTick VAl : ");
+		kprintf((uint8_t *)"%d",(uint8_t *) &tickCount);
+		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"mscount : ");
+		kprintf((uint8_t *)"%d",(uint8_t *) &timeMS);
+		kprintf((uint8_t *)"%s",(uint8_t *) &"  ");
+		kprintf((uint8_t *)"%s",(uint8_t *) &"COUNTFLAG : ");
+		kprintf((uint8_t *)"%c",(uint8_t *) &stat);
+		kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
+		stat = SysTick_GetStatus() + '0';
+		tickCount = SysTick_GetCount();
+		timeMS = SysTickGetTime();
+	}	
+	kprintf((uint8_t *)"%s",(uint8_t *) &"\n");
+}
 
 void kmain(void) {
 	__sys_init();
 
 	// title
-	kprintf((uint8_t*)"%s",(uint8_t*)"###########################################\n", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"Booting OS CSE: ", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"Version: 1.0\n", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"###########################################\n", "\n");
-	kprintf((uint8_t*)"%s",(uint8_t*)"Welcome ...", "\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"###########################################\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Booting OS CSE: ");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Version: 1.0\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"###########################################\n");
+	kprintf((uint8_t*)"%s",(uint8_t*)"Welcome ...");
 
 	SysTick_enable();
 	int startTime = 0;
@@ -154,18 +154,18 @@ void kmain(void) {
 
 	// loop
 	while(1){
-		demo_kprintf_kscanf();
+		// demo_kprintf_kscanf();
 		// demo_systick();
 
-		// startTime = SysTick_getTime();
+		startTime = SysTick_getTime();
 
-		// kprintf((uint8_t*)"%s",(uint8_t*)"Press Enter to Get Time (ms)");
-		// kscanf((uint8_t*)"%c", &c);
+		kprintf((uint8_t*)"%s",(uint8_t*)"Press Enter to Get Time (ms)");
+		kscanf((uint8_t*)"%c", &c);
 		
-		// endTime = SysTick_getTime();
-		// time += endTime - startTime;
+		endTime = SysTick_getTime();
+		time += endTime - startTime;
 
-		// kprintf((uint8_t*)"%d",(uint8_t*)&time);
+		kprintf((uint8_t*)"%d",(uint8_t*)&time);
 
 		//you can change the following line by replacing a delay function
 		for(uint32_t i=0;i<1000000;i++){}
