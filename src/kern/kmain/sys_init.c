4826b7817e2d49c71b17888f95dda011b78a8d28
smmehrab <mehrab.24csedu.001@gmail.com>
2022-08-31 22:19:19 +0600

    sys_init.c - modified - called SysTick_init()

--- src/kern/kmain/sys_init.c
+++ src/kern/kmain/sys_init.c
@@ -3,6 +3,7 @@
 #include "../arch/stm32f446re/include/dev/usart.h"
 #include "../arch/stm32f446re/include/dev/gpio.h"
 #include "../arch/stm32f446re/include/sys/stm32_peps.h"
+#include "../arch/stm32f446re/include/sys/sys.h"
 
 // turn on the fpu
 void DRV_FPU_ACTIVE(void){
@@ -11,6 +12,8 @@ void DRV_FPU_ACTIVE(void){
 
 void __sys_init(void) {
 	DRV_CLOCK_INIT(); //configure system clock 180 MHz
+	// For the SysTick Exception Handler to be called, every 10ms
+	SysTick_init(10);
 	DRV_GPIO_INIT(GPIOA);
 	DRV_USART_INIT(USART2); //configure as standard input and output 
 	DRV_FPU_ACTIVE();
