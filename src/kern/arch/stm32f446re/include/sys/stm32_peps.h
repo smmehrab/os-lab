2a022427f4bf60efefb28a5e2a004e45bec6c9b5
smmehrab <mehrab.24csedu.001@gmail.com>
2022-09-12 23:09:50 +0600

    previous SCB_type commented out

--- src/kern/arch/stm32f446re/include/sys/stm32_peps.h
+++ src/kern/arch/stm32f446re/include/sys/stm32_peps.h
@@ -208,26 +208,26 @@ uint32_t volatile OR;   /*!< TIM option register, Address offset: 0x50 */
 } TIM_TypeDef;
 
 //define SCB data struct
-typedef struct {
-  uint32_t CPUID;
-  uint32_t ICSR;
-  uint32_t VTOR;
-  uint32_t AIRCR;
-  uint32_t SCR;
-  uint32_t CCR;
-  uint32_t SHPR1;
-  uint32_t SHPR2;
-  uint32_t SHPR3;
-  uint32_t SHCSR;
-  uint32_t CFSR;
-  uint32_t HFSR;
-  uint32_t MMAR;
-  uint32_t BFAR;
-  uint32_t AFSR;
-  uint8_t reserved[76];
-  uint32_t CPACR;
-}SCB_type;
-#define SCB ((SCB_type *) 0xE000ED00 ) 
+// typedef struct {
+//   uint32_t CPUID;
+//   uint32_t ICSR;
+//   uint32_t VTOR;
+//   uint32_t AIRCR;
+//   uint32_t SCR;
+//   uint32_t CCR;
+//   uint32_t SHPR1;
+//   uint32_t SHPR2;
+//   uint32_t SHPR3;
+//   uint32_t SHCSR;
+//   uint32_t CFSR;
+//   uint32_t HFSR;
+//   uint32_t MMAR;
+//   uint32_t BFAR;
+//   uint32_t AFSR;
+//   uint8_t reserved[76];
+//   uint32_t CPACR;
+// }SCB_type;
+// #define SCB ((SCB_type *) 0xE000ED00 ) 
 
 
 #endif
